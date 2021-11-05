dc3e9387bf3d889827ddb9157fe513360b08a545
BOB <you@example.com>
2021-11-05 09:30:35 +0530

    skelton

--- microsoft/virtualmachine/main.tf
+++ microsoft/virtualmachine/main.tf
@@ -1,4 +1,15 @@
-resource "azurerm_network_interface" "nic" {
+resource "azurerm_network_interface" "agentnic" {
+  name                = "agent-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+resource "azurerm_network_interface" "workloadnic" {
   name                = "example-nic"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
@@ -10,14 +21,14 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "linvm" {
+resource "azurerm_linux_virtual_machine" "liagentnvm" {
   name                = "example-machine"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   size                = "Standard_F2"
   admin_username      = "adminuser"
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.agentnic.id,
   ]
 
   admin_ssh_key {
@@ -36,4 +47,24 @@ resource "azurerm_linux_virtual_machine" "linvm" {
     sku       = "16.04-LTS"
     version   = "latest"
   }
+}
+
+resource "azurerm_virtual_machine_extension" "vm1extension" {
+  name                 = var.vmName
+  virtual_machine_id   = azurerm_linux_virtual_machine.liagentnvm.id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.1"
+
+  settings = <<SETTINGS
+    {
+        "fileUris":["https://raw.githubusercontent.com/globalbao/terraform-azurerm-ansible-linux-vm/master/scripts/ubuntu-setup-ansible.sh"]
+    }
+SETTINGS
+
+  protected_settings = <<PROTECTED_SETTINGS
+    {
+        "commandToExecute": ". ./ubuntu-setup-ansible.sh"
+    }
+PROTECTED_SETTINGS
 }
